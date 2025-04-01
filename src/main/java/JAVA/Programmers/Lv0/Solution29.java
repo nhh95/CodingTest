5dd67dbbe13024ebedd46247da7af9d72018d24d
nam <namhh0101001@gmail.com>
2025-04-01 10:15:12 +0900

    .

--- src/main/java/JAVA/Programmers/Lv0/Solution29.java
+++ src/main/java/JAVA/Programmers/Lv0/Solution29.java
@@ -35,8 +35,19 @@ public class Solution29 {
 	
 	
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = {};
-        return answer;
+        
+        for(int i = 0; i < queries.length; i++){
+            int j = queries[i][0];
+            int k = queries[i][1];
+            
+            int temp = arr[j];
+            
+            arr[j] = arr[k];
+            arr[k] = temp;
+            
+        }
+        
+        return arr;
     }
 }
 
